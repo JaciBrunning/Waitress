17d8830be2a90c856c60c4a323207633249baaf3
Jaci Brunning <jaci.brunning@gmail.com>
2015-09-30 13:41:42 +0800

    Expand testcase and chmod +x

--- test/query_test.rb
+++ test/query_test.rb
@@ -12,7 +12,9 @@ class QueryTest < Test::Unit::TestCase
     tests = {
       "test=true" => {"test" => "true"},
       "test=true&test=false" => { "test" => "false" },
-      "test[]=true&test[]=false" => { "test" => ["true", "false"] }
+      "test[]=true&test[]=false" => { "test" => ["true", "false"] },
+      "test[=true&test[=false" => { "test[" => "false" },
+      "test[]other=true&test[]another=false" => { "test" => [{"other" => "true", "another" => "false"}] }
     }
     tests.each do |key, val|
       assert_equal(val, Waitress::QueryParser.parse(key))
